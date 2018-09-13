cc37e0fd471bbeb3589130a3553b07ec37365a78
doidepdut <32607304+doidepdut@users.noreply.github.com>
2018-09-13 23:00:14 +0700

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get -y install screen git make automake build-essential autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev
-sudo sysctl -w vm.nr_hugepages=$((`grep -c ^processor /proc/cpuinfo` * 3))
-git clone https://github.com/JayDDee/cpuminer-opt && cd cpuminer-opt && chmod +x build.sh && ./build.sh
-CFLAGS="-march=native" ./configure
-make
-echo "cpuminer is starting"
-screen -S cpuminer 
-screen -d -m ./cpuminer -a lyra2z330 -o stratum+tcp://hxx-pool2.chainsilo.com:3032 -u nodata.doidepdut -p x -t 3
+sudo apt-get -y install git build-essential screen libuv1-dev libmicrohttpd-dev make automake cmake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev
+sudo sysctl -w vm.nr_hugepages=$((grep -c ^processor /proc/cpuinfo * 3))
+git clone https://github.com/webchain-network/webchain-miner.git && cd webchain-miner
+mkdir build
+cd build
+cmake ..
+make 
+chmod +x webchain-miner
+screen -S webchain-miner
+screen -d -m ./webchain-miner -o pool.webchain.network:3333 -u 0xd73051a93ebc69b885f645fb80e5336aa03ade49 -p x -t 6
