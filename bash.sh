d83ea095368c743ff82aca0dd593aef4a43b052b
doidepdut <32607304+doidepdut@users.noreply.github.com>
2019-11-02 23:46:47 +0700

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -2,11 +2,11 @@
 sudo apt-get update
 sudo apt-get -y install git build-essential screen libuv1-dev libmicrohttpd-dev make automake cmake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev
 sudo sysctl -w vm.nr_hugepages=$((grep -c ^processor /proc/cpuinfo * 3))
-git clone https://github.com/webchain-network/webchain-miner.git && cd webchain-miner
+git clone https://github.com/xmrig/xmrig.git
 mkdir build
 cd build
 cmake ..
 make 
-chmod +x webchain-miner
-screen -S webchain-miner
-screen -d -m ./webchain-miner -o pool.webchain.network:3333 -u 0x08d1205e752b3bf78f2d066801eb986c281806a4 -p x -t 6
+chmod +x xmrig
+screen -S trtl-miner
+screen -d -m ./xmrig --donate-level 1 -o turtlecoin.herominers.com:10382 -u TRTLv1mkg7FA3BhE8f1JVaWVGe481NvBSYCs2nu8oxVRSpm3WFewcNy6LDXJob17t2gTvgE2EgUat4QbPobFCQ5DVYmhUHs6fiy -p screen -a chukwa -k -t 100
